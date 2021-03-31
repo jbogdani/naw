1e4d54d59dda353a526d64b5b0fc1ec49da046ef
Julian Bogdani <jbogdani@gmail.com>
2021-03-31 16:03:32 +0200

    sistemato testo del popup della mappa

--- js/mappa.js
+++ js/mappa.js
@@ -25,6 +25,7 @@ const mappa = () => {
             "from": 1973,
             "to": 2004,
             "post-it": "Scavi: 2005-2010 Università di Torino; 2011 - oggi: Ca'Foscari Università di Venezia (https://sites.google.com/view/egittologiavenezia/scavo)",
+            "post-es": "Excavations: 2005-2010 University of Turin; 2011 - currently: Ca'Foscari University of Venice (https://sites.google.com/view/egittologiavenezia/scavo)",
             "url": ""
         },
         {
@@ -98,10 +99,17 @@ const mappa = () => {
 
     if (typeof L === 'undefined'){
         console.log('Load Leflet to build map: https://unpkg.com/leaflet@1.7.1/dist/leaflet.js');
+        return;
     }
 
     if (typeof document.getElementById('map') === 'undefined'){
         console.log('Add element <div id="map"></div> in the document to build the map');
+        return;
+    }
+    let lang = document.getElementById('map').textContent;
+    if (lang !== 'it' && lang !== 'en'){
+        lang = 'it';
+        console.log('Language set to `it` by default. Please explicitly set language by adding `it` or `en` as content of div#map');
     }
 
     var map = L.map('map').setView([20.024509, 31.471851], 6);
@@ -113,6 +121,8 @@ const mappa = () => {
 
     pois.forEach(p => {
         let isCurrent;
+        let submergedText = p.submerged ? (lang === 'it' ? '<p>Il sito è oggi sommerso</p>' : '<p>The site is today submerged</p>') : '';
+        let fromToText = lang === 'it' ? `<p><strong>Attività Sapenza</strong><br> ${p.from} – ${p.to}</p>` : `<p><strong>Sapienza activities</strong><br> ${p.from} — ${p.tp}</p>`;
         if (window.location.href.indexOf(`/${p.url}.html`) > -1){
             isCurrent = true;
         }
@@ -122,7 +132,12 @@ const mappa = () => {
             fillOpacity: 0.5,
             radius: 5
         }).addTo(map)
-            .bindPopup(`<h5><a href="./${p.url}">${p.name}</a></h5>`);
+            .bindPopup(`<h5 class="text-center"><a href="./${p.url}" title="${p.name}">${p.name}</a></h5>
+            <div class="text-center">
+                ${submergedText}
+                ${fromToText}
+            </div>
+            `);
         if (isCurrent){
             marker.openPopup();
         }
